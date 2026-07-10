882ec7f4592bd4914f7b1a8774b6fe895315ca71
xw2781 <xw2781@gmail.com>
2026-07-10 14:50:29 -0400

    fix(migration): correct Bootstrap method name

--- python-api/migration/resq_migration/core.py
+++ python-api/migration/resq_migration/core.py
@@ -16,7 +16,7 @@ METHOD_TYPE_NAMES = {
     METHOD_TYPE_BF_CODE: "Bornhuetter Ferguson",
     3: "Cape Cod",
     METHOD_TYPE_RESULT_SELECTION_CODE: "Result Selection",
-    6: "Boottrap",
+    6: "Bootstrap",
     7: "Stochastic Consolidation",
     8: "Berquist Sherman"
 }
